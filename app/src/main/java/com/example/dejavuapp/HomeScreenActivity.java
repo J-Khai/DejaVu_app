dea717275f41625c3e7fa3717b82300c57e06f79
J-Khai <jaheimyt@gmail.com>
2022-03-18 14:27:27 -0400

    HelpScreen and some new updates

--- app/src/main/java/com/example/dejavuapp/HomeScreenActivity.java
+++ app/src/main/java/com/example/dejavuapp/HomeScreenActivity.java
@@ -3,12 +3,27 @@ package com.example.dejavuapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class HomeScreenActivity extends AppCompatActivity {
+    ImageButton settingsBtn, leaderBoardBtn, helpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+
+        settingsBtn = findViewById(R.id.settingsBtn); //gets the id
+
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
