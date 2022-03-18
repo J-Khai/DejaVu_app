b1249325d1d6444a92779011a500d6136691da26
ankitmodhera <ankitmodhera24@gmail.com>
2022-03-18 12:43:16 -0400

    hell

--- app/src/main/java/com/example/dejavuapp/MainActivity.java
+++ app/src/main/java/com/example/dejavuapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Home_Screen);
+        setContentView(R.layout.home_screen);
     }
 }
